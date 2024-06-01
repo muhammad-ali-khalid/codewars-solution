f1f52dfc0c28896725f13af33962ecea0b574353
Muhammad Ali Khalid <148471160+muhammad-ali-khalid@users.noreply.github.com>
2024-06-02 02:29:37 +0500

    Create even-or-odd.cpp

---
+++ 8kyu/even-or-odd.cpp
@@ -0,0 +1,12 @@
+/*
+
+Kata can be found at: https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+*/
+#include <string>
+
+std::string even_or_odd(int number) 
+{
+  if (number % 2 == 0) return "Even";
+  else return "Odd";
+}
